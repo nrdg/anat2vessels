51d3e948fb3ebc97ef3c28547b172d86f2a041ad
Ariel Rokem <arokem@gmail.com>
2026-06-10 14:58:46 -0700

    Test just importing the modules for now.

--- test/test_anat2vessels.py
+++ test/test_anat2vessels.py
@@ -1,2 +1,10 @@
+from anat2vessels import features as avf
+from anat2vessels import preprocess as avp
+from anat2vessels import vessels2csv as csv
+
+
 def test_smoke():
+    avf
+    avp
+    csv
     pass
