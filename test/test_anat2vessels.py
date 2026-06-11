dcc9c568babf7c8cdef5d2e8b38132b53cc81931
Ariel Rokem <arokem@gmail.com>
2026-06-11 09:12:18 -0700

    Update smoke test to include fetch module

--- test/test_anat2vessels.py
+++ test/test_anat2vessels.py
@@ -1,10 +1,13 @@
 from anat2vessels import features as avf
 from anat2vessels import preprocess as avp
 from anat2vessels import vessels2csv as csv
+from anat2vessels.data import fetch_ref_img, fetch_test_data
 
 
 def test_smoke():
     avf
     avp
     csv
+    fetch_ref_img
+    fetch_test_data
     pass
